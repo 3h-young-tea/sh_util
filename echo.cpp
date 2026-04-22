26ea5bd90bac9967bd9f0dd520ea0472bc61e52d
young_tea <3h.young.tea@gmail.com>
2026-04-22 13:51:09 +0800

    Fix version output and return statement in solve

--- echo.cpp
+++ echo.cpp
@@ -18,7 +18,7 @@ void	solve(const std::vector<std::string> &argz) {
 	std::string version = "echo (lil_tea) 7890\nPackaged by lil_tea (7890)\nCopyright (C) 1995 Free Software Foundation, Inc.\nLicense AGPLv3+: GNU AGPL version 3 or later <https://github.com/3h-young-tea/sh_util>.\nThis is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.";
 
 	if (argz.size() == 0uz)
-		return std::println(std::cerr, "{}", version);
+		return std::println("");
 
 	for (const std::string &s : argz)
 		if (to_lower(s) == "--version")
