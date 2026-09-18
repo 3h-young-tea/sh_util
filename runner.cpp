0514d70ba2f393e9600769d3c7678981507fdbe9
young_tea <3h.young.tea@gmail.com>
2026-09-18 17:34:53 +0800

    Refactor input handling and command execution

--- runner.cpp
+++ runner.cpp
@@ -19,12 +19,23 @@ void	solve(const std::vector<std::string> &argz)
 	pre(1uz < argz.size()) {
 	std::println("");
 
+	std::println("{} input start", std::string(16uz, '-'));
+
+	std::string full_token, token;
+
+	while (std::cin >> token)
+		full_token += token + " ";
+
+	std::println("{} input term", std::string(16uz, '-'));
+
+	std::println("");
+
 	if (argz[1uz].substr(argz[1uz].length() - 4uz) == ".elf")
 		auto _ = std::system(std::format("echo \"elf: $(du -k '{}' | cut -f1)kib\"", argz[1]).c_str());
 
-	std::println("{} start", std::string(16uz, '-'));
+	std::println("{} runnin start", std::string(16uz, '-'));
 
-	std::string cmd = std::format("/usr/bin/time -f '\n{} term\ntime: %esec\nram: %Mkib'", std::string(16uz, '-'));
+	std::string cmd = std::format("echo {} | /usr/bin/time -f '\n{} runnin term\ntime: %esec\nram: %Mkib'", full_token, std::string(16uz, '-'));
 
 	for (std::size_t x = 1uz; x < argz.size(); ++x)
 		if (argz[x][argz[x].length() - 1uz] == '<' || argz[x][argz[x].length() - 1uz] == '>')
@@ -32,7 +43,8 @@ void	solve(const std::vector<std::string> &argz)
 		else
 			cmd = std::format("{} \"{}\"", cmd, argz[x]);
 
-	std::println("ret: {}\n", WEXITSTATUS(std::system(cmd.c_str())));
+	auto x = std::system(cmd.c_str());
+	std::println("ret: {}\n", WEXITSTATUS(x));
 }
 
 signed	main(int argc, char **argv) {
